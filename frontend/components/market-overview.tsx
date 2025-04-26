4b944fc126e77666824ad8ec6fea5d5e37779583
Alissa <alissawu77@gmail.com>
2025-04-26 02:56:07 -0700

    add padding at the left of the market performancfe graph

--- frontend/components/market-overview.tsx
+++ frontend/components/market-overview.tsx
@@ -91,7 +91,8 @@ export function MarketOverview({ type }: MarketOverviewProps) {
             </div>
             <div className="h-[250px] mt-2">
               <ResponsiveContainer width="100%" height="100%">
-                <AreaChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                {/*add some padding at the left so it doesn't look cramped at 5figs*/}
+                <AreaChart data={data} margin={{ top: 10, right: 30, left: 20, bottom: 0 }}>
                   <defs>
                     <linearGradient id="colorValue" x1="0" y1="0" x2="0" y2="1">
                       <stop offset="5%" stopColor="#22c55e" stopOpacity={0.8} />
