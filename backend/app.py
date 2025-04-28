cef87cb4b4722a68fbf7c692f6e2ad093ec9713c
Sirui Li <LiSiruiRay@outlook.com>
2025-04-28 13:41:00 -0400

    updated app.py

--- backend/app.py
+++ backend/app.py
@@ -382,4 +382,8 @@ def clear_cache():
 
 if __name__ == "__main__":
     # Start the Flask app
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    # app.run(debug=True, host='0.0.0.0', port=5001)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
